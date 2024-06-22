694050d65284d5b74503815bd2e264b4400bad71
ShankarAswal <shankaraswal@gmail.com>
2024-06-22 17:34:48 +0100

    error card using card wrapper (#16)

--- app/page.tsx
+++ app/page.tsx
@@ -7,9 +7,9 @@ export default function Home() {
       className="flex h-full flex-col items-center justify-center 
     bg-[radial-gradient(ellipse_at_top,_var(--tw-gradient-stops))] from-red-200 to-red-500"
     >
-      <div className="space-y-6 text-center border-2 p-10">
+      <div className="space-y-6 text-center border-2 bg-slate-100/20 shadow-xl rounded-xl p-10">
         <h1 className="text-6xl font-semibold text-white drop-shadow-lg">
-          ❖ Login From
+          ❖ Login Form
         </h1>
         <p className="text-white text-lg text-center">
           Please login with user name and password !
