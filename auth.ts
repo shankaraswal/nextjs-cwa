bb872ac331cac12bfbb822083ebc27991182bd9c
Shankar Aswal <shankaraswal@gmail.com>
2024-06-22 02:37:30 +0100

    added: auto email verified if user login with oauth and set user role ADMIN

--- auth.ts
+++ auth.ts
@@ -10,6 +10,19 @@ export const {
   signOut,
   handlers: { GET, POST },
 } = NextAuth({
+  events: {
+    async linkAccount({ user }) {
+      await db.user.update({
+        where: {
+          id: user.id,
+        },
+        data: {
+          emailVerified: new Date(),
+          role: "ADMIN",
+        },
+      });
+    },
+  },
   callbacks: {
     async session({ session, token }) {
       if (token.sub && session.user) {
